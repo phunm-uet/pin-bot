2bf20fb5bd9107ded87d4955d356054a3f013d12
phunm-uet <phuapplehmu@gmail.com>
2017-08-24 20:31:46 +0700

    Remove config file

--- config.example.php
+++ config.example.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pass = 'minhphu';
-$db_name = 'tmz';
+$db_pass = 'xxx';
+$db_name = 'xxx';
 $conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 ?>
